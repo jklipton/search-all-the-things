4e25a936d9f7a7c139a6d38972b9ac84204057e4
Jen Lipton <jkl.lipton@gmail.com>
2018-05-31 15:57:52 -0700

    responsive n shit

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -41,8 +41,9 @@ export default class Articles extends Component {
     )
 
     if (images.length === 0) return (
-      <section id="dog-images" className="intro"> 
-        <p><h2>Hey, you like dogs?  We got dogs!</h2> Choose your favorite breed from the dropdown menu!</p>
+      <section id="dog-images" className="intro">
+        <p className="wide"></p>
+        <p className="wide"><h2>Hey, you like dogs?  We got dogs!</h2> Choose your favorite breed from the dropdown menu!</p>
         {gifs.map(gif => <p className="gif" style={{ backgroundImage: `url(${gif})` }}></p>)}
       </section>
     )
